58b30aae91f87126b0c3d8cff1c794c5158b64fc
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-20 09:56:41 +0300

    Added Footer and use custom webpack for use svg

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { mainFont } from "@/libs/font"
 import "@/styles/reset.css"
 import "@/styles/variables.css"
 import "@/styles/global.css"
+import { Footer } from "@/components/footer"
 
 export default function RootLayout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
       <body className="layout">
         <div aria-hidden="true" className="overlay" />
         {children}
+        <Footer />
       </body>
     </html>
   )
