0320e0db4624213ae5961afbbe1567c86c4dbfc3
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-04-20 09:15:59 +0300

    Added Font

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { mainFont } from "@/libs/font"
 import "@/styles/reset.css"
 import "@/styles/variables.css"
 import "@/styles/global.css"
@@ -9,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={mainFont.className}>
       <body className="layout">
         <div aria-hidden="true" className="overlay" />
         {children}
